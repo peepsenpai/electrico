8a3d2b900ad5ded50f6f91d65ca96bf03798d381
Arijit <arijitmaiti085@gmail.com>
2021-11-29 17:50:32 +0530

    initial commit

--- logic.js
+++ logic.js
@@ -3,3 +3,16 @@
 //     let val = range.value;
 //     console.log(val);
 // })
+
+
+// headers active menu 
+let navItem = document.querySelectorAll('.nav-item');
+navItem.forEach((e) => {
+
+    e.addEventListener('click', (clickItem) => {
+        navItem.forEach((perItem) => {
+            perItem.classList.remove('active')
+        })
+        clickItem.path[1].classList.add('active');
+    })
+})
