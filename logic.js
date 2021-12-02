992c70a7960f1848ff5120f6387c4f1b79b63121
Arijit <arijitmaiti085@gmail.com>
2021-12-02 19:49:33 +0530

    added changes

--- logic.js
+++ logic.js
@@ -1,8 +1,24 @@
-// let range = document.getElementById('range');
-// range.addEventListener('change', ()=>{
-//     let val = range.value;
-//     console.log(val);
-// })
+// ac 
+let range = document.getElementById('customRange1');
+let spanValue = document.getElementById('ac-js-value')
+range.addEventListener('change', ()=>{
+    let val = range.value;
+    spanValue.innerText = val;
+})
+
+// ac boxes
+const acCacltBox = document.querySelectorAll('.ac-calct-box');
+acCacltBox.forEach((e,i)=>{
+    let airHeading = e.querySelector('.ac-calct-heading');
+    airHeading.addEventListener('click', ()=>{
+        acCacltBox.forEach((e2)=>{
+            if(e !== e2){
+                e2.classList.remove('active')
+            }
+        })
+        e.classList.toggle('active')
+    })
+})
 
 
 // headers active menu 
@@ -16,3 +32,12 @@ navItem.forEach((e) => {
         clickItem.path[1].classList.add('active');
     })
 })
+
+// upper-header-toggle
+const mobileToggle = document.getElementById('mobile-header-bar'),
+    headerToggleRight = document.getElementById('header-right-box-toggle');
+mobileToggle.addEventListener('click', ()=>{
+    headerToggleRight.classList.toggle('active');
+})
+
+
